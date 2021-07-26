3da470e4a3b8af339d5bb94a05064b01cf201664
cryptoAlgo-win <support@cryptoalgo.cf>
2021-07-26 21:06:41 +0800

    Changed template to compile in Windows

--- src/defaultLangData/compileRunCmds.js
+++ src/defaultLangData/compileRunCmds.js
@@ -1,17 +1,28 @@
+/*
+Compiler and running commands for built-in languages
+Syntax:
+[langCode]: {
+  [os | 'other']: {
+    compile: [
+      { cmd: 'program', args: [...programArgs] },
+      ...
+    ],
+    run: { cmd: 'compiledProgram', args: [...args] }
+  }
+}
+Code is provided in stdin when running each compile stage,
+and is also present in the working dir as a file 'code'
+(during both run and compile stages)
+ */
+
 const compileRunCmd = {
     cpp: {
-        darwin: {
+        other: {
             compile: [
                 { cmd: 'g++', args: ['-x', 'c++', 'code', '-o', 'out', '-Wall', '-Wshadow',
-                    '-O2', '-lm', '-m64', '-s', '-w', '-std=gnu++17', '-fmax-errors=512'] },
+                        '-O2', '-lm', '-m64', '-s', '-w', '-std=gnu++17', '-fmax-errors=512'] },
             ],
             run: {cmd: '$dir/out', args: []}
-        },
-        win32: {
-            compile: []
-        },
-        other: {
-
         }
     },
     python3: {
